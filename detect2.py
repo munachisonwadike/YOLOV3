bd382b67324b4a6ed74ffb64282edad16bc3f057
munachisonwadike <22077758+munachisonwadike@users.noreply.github.com>
2019-10-15 18:13:11 +0400

    edit detect

--- detect2.py
+++ detect2.py
@@ -8,19 +8,23 @@ from utils.utils import *
 
 
 
-def detect(save_txt=False):
-    img_size = args.img_size  # (height, width) =(320, 192) or (416, 256) or (608, 352)  
-    out, source, weights, view_img = args.output, args.source, args.weights, args.view_img
+def detect():
+    """
+    Generates detections from images and saves the images to output file
+    Input image is references with --source flag
+    """
+    image_size = args.image_size  # (height, width) =(320, 192) or (416, 256) or (608, 352)  
+    out_folder, source_image, weights = args.output_folder, args.source, args.weights 
 
     # Delete output folder and make new folder to output the detection image
-    if os.path.exists(out):
-        shutil.rmtree(out) #imported from datasets.py 
-    os.makedirs(out)   
+    if os.path.exists(out_folder):
+        shutil.rmtree(out_folder) #imported from datasets.py 
+    os.makedirs(out_folder)   
 
     device = torch_utils.select_device(args.device)
 
     # Set YOLOV3 model 
-    model = YOLOV3(args.cfg, img_size)
+    model = YOLOV3(args.cfg, image_size)
 
     # Check if weights file is to be gotten online
     try_download(weights) #specific files names to test are defined in models.try_dowload
@@ -37,88 +41,76 @@ def detect(save_txt=False):
     
 
     # set the image loader, and load the classes and colors
-    dataset = ImageLoader(source, img_size=img_size)
+    dataset = ImageLoader(source_image, image_size=image_size)
     classes_list = classes_load(parse_data_cfg(args.data)['names'])
     color_list = [[random.randint(0, 255) for i in range(3)] for class_ in range(len(classes_list))]
 
-    ###### need to change variable names, and also flag names, but not the flag names so as to change
-    #them in train and test as well and not the changes in the google doc?
+    
+    # Inference over the images in path
+    start_time = time.time()
 
-    # Run inference
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        t = time.time()
+    for path, image, image_originals, vid_cap in dataset:
+        current_time = time.time()
 
         # Get detections
-        img = torch.from_numpy(img).to(device)
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
-        pred, _ = model(img)
+        image = torch.from_numpy(image).to(device)
+        if image.ndimension() == 3:
+            image = image.unsqueeze(0)
+        prediction, _ = model(image)
 
-        for i, det in enumerate(non_max_suppression(pred, args.conf_thres, args.nms_thres)):  # detections per image
-            p, s, im0 = path, '', im0s
+        # Run non max suppression on the detection, display, and write image to output file
+        for index, detection in enumerate(non_max_suppression(prediction, args.conf_threshold, args.nms_threshold)):  # detections per image
+            
+            # Note that shape of detection, since it is for one image, is [numboxes, (x1, y1, x2, y2, object_conf, class_conf, class)]
+            pth, detection_names, original_image = path, '', image_originals  
+ 
+            save_path = str(Path(out_folder) / Path(pth).name) # path to save the output image
+            detection_names += '%gx%g ' % image.shape[2:]  # add image size to image description
 
-            save_path = str(Path(out) / Path(p).name)
-            s += '%gx%g ' % img.shape[2:]  # print string
-            if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            
+            # If there is a detection available
+            if detection is not None and len(detection):
+                # Change box dimensions from image_size to original_image size
+                detection[:, :4] = box_scale(image.shape[2:], detection[:, :4], original_image.shape).round()
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += '%g %ss, ' % (n, classes_list[int(c)])  # add to string
+                
+                # For each class, add the class name and number of detections to the terminal output
+                for class_ in detection[:, -1].unique():
+                    num_dets = (detection[:, -1]==class_).sum()  
+                    detection_names += '%g %ss, ' % (num_dets, classes_list[int(class_)])  # add to string
+
 
                 # Write results
-                for *xyxy, conf, _, cls in det:
-                    if save_txt:  # Write to file
-                        with open(save_path + '.txt', 'a') as file:
-                            file.write(('%g ' * 6 + '\n') % (*xyxy, cls, conf))
+                for *x1y1x2y2, obj_conf, _, class_ in detection:
 
-                    # Add bbox to image
-                    label = '%s %.2f' % (classes_list[int(cls)], conf)
-                    plot_one_box(xyxy, im0, label=label, color=color_list[int(cls)])
+                    # Bounding box addition
+                    obj_label = '%s %.2f' % (classes_list[int(class_)], obj_conf)
+                    single_box_plot(x1y1x2y2, original_image, obj_label=obj_label, color=color_list[int(class_)])
 
-            print('%sDone. (%.3fs)' % (s, time.time() - t))
+            print('%sDetection #%d complete. (%.3fs)' % (detection_names, index,  time.time() - current_time))
 
-            # Stream results
-            if view_img:
-                cv2.imshow(p, im0)
+            # Show and save image detection results
+            cv2.imshow(pth, original_image)
+            cv2.imwrite(save_path, original_image)
 
 
-            # Save image detection results 
-            if dataset.mode == 'images':
-                cv2.imwrite(save_path, im0)
-            else:
-                 
-                vid_path = save_path
+    print('Results saved to %s' % os.getcwd() + os.sep + out_folder)
 
-                fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*args.fourcc), fps, (w, h))
-               
-                vid_writer.write(im0)
-
-    
-    print('Results saved to %s' % os.getcwd() + os.sep + out)
-
-    print('Done. (%.3fs)' % (time.time() - t0))
+    print('All detections complete. (%.3fs)' % (time.time() - start_time))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser() 
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='cfg file path')
+    parser.add_argument('--conf-threshold', type=float, default=0.3, help='object confidence threshold')
     parser.add_argument('--data', type=str, default='data/coco.data', help='coco.data file path')
-    parser.add_argument('--weights', type=str, default='weights/best.pt', help='path to weights file')
-    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
-    parser.add_argument('--nms-thres', type=float, default=0.5, help='iou threshold for non-maximum suppression')
-    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--image-size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--nms-threshold', type=float, default=0.5, help='iou threshold for non-maximum suppression')
+    parser.add_argument('--output-folder', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam    
+    parser.add_argument('--weights', type=str, default='weights/best.pt', help='path to weights file')
+
     args = parser.parse_args()
     print(args)
 
